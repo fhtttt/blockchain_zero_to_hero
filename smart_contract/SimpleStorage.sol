2da0e586b5ad2514d1c4e07350612414ea459a23
fhtttt <haotian.fang.swiss@gmail.com>
2026-01-21 16:19:49 +0800

    function

--- smart_contract/SimpleStorage.sol
+++ smart_contract/SimpleStorage.sol
@@ -2,10 +2,18 @@
 pragma solidity ^0.8.19;
 
 contract SimpleStorage {
-    bool hasFavoriteNumber = true;
-    int32 num = -1;
-    uint32 num1 = 2;
-    string name = "Haotian";
-    bytes32 name1 = "Haotian";
-    address addr1 = 0xd57C37C86e54661fD8E0398f0Db320C62bE6927f;
+    //Functions
+    uint256 public favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+        // if you add the retrieve here, you will cost a bit more
+        retrieve();
+    }
+
+    // view pure
+    function retrieve() public view returns (uint256) {
+        // this function won't cost when you call
+        return favoriteNumber;
+    }
 }
--- smart_contract/foundation.md
+++ smart_contract/foundation.md
@@ -2,6 +2,6 @@
 
 用Remix来了解smart contract 语法
 
-Remix: https://remix.polkadot.io/
+Remix: https://remix.live/
 
 SimpleStorage.sol
