3aff1e2d79a28f5d8d6fa2f3263105b14231df91
fhtttt <haotian.fang.swiss@gmail.com>
2026-01-21 14:57:06 +0800

    Basic Variable Test

---
+++ smart_contract/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SimpleStorage {
+    bool hasFavoriteNumber = true;
+    int32 num = -1;
+    uint32 num1 = 2;
+    string name = "Haotian";
+    bytes32 name1 = "Haotian";
+    address addr1 = 0xd57C37C86e54661fD8E0398f0Db320C62bE6927f;
+}
---
+++ smart_contract/foundation.md
@@ -0,0 +1,7 @@
+# Foundation
+
+用Remix来了解smart contract 语法
+
+Remix: https://remix.polkadot.io/
+
+SimpleStorage.sol
