b06dfc8d76530841f0ded1e417f6d0926d25673b
fhtttt <haotian.fang.swiss@gmail.com>
2026-01-21 17:01:12 +0800

    mapping

--- smart_contract/SimpleStorage.sol
+++ smart_contract/SimpleStorage.sol
@@ -11,11 +11,13 @@ contract SimpleStorage {
     // dynamic size array
     Person[] public listOfPeople;
 
+    mapping(string => uint256) public nameToFavoriteNumber;
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push(Person(_favoriteNumber, _name));
         _name = "hai";
-    }
+        nameToFavoriteNumber[_name] = _favoriteNumber; //use mapping we can just index the name to get the favorite number
 
     //memory, calldata, storage
     // Calldata you cannot modify, memory you can modify
+    //
 }
